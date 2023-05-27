61653695c66c94c23c7dc0c887d5db7c348c6868
Neerajpatelcoder <pnrvns2000@gmail.com>
2023-05-27 15:52:17 +0530

    complete login signup page

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:auth_page/register.dart';
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
